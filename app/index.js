0f9de0c6df9a08c1e79f570a549b0fb250fc9134
Nathen Drees <nadrees@gmail.com>
2017-03-15 19:44:00 -0500

    Added react

--- app/index.js
+++ app/index.js
@@ -1,13 +1,7 @@
-/**
- * Created by Nathen on 3/12/2017.
- */
-function component () {
-    var element = document.createElement('div');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-    /* lodash is required for the next line to work */
-    element.innerHTML = 'Hello, Webpack';
-
-    return element;
-}
-
-document.body.appendChild(component());
+ReactDOM.render(
+    <h1>Hello, React!</h1>,
+    document.getElementById('container')
+);
